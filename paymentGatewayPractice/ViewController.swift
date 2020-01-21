348de657a0a0ee0743bad889157320601d3cf090
Varalakshmi <varalakshmi1318@gmail.com>
2020-01-21 12:25:41 +0530

    Initial Commit

---
+++ paymentGatewayPractice/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  paymentGatewayPractice
+//
+//  Created by Varalakshmi Kacherla on 1/21/20.
+//  Copyright © 2020 Varalakshmi Kacherla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    var actorName = "Varun"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
